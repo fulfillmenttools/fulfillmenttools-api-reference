b2100dd800a23ead52e3d53683a06511f8017c37
FFT-Github <FFT-Github@users.noreply.github.com>
2021-03-04 15:12:07 +0000

    deploy: 2e37906ba6d14c15a53cb31d25dc7eb78587c671

--- api-ctl.sh
+++ api-ctl.sh
@@ -7,6 +7,9 @@ apiVersion=$2
 
 apiUrl="https://raw.githubusercontent.com/fulfillmenttools/fulfillmenttools-api-reference/${apiVersion}/api.swagger.yaml"
 
+swaggerCodeGen="https://repo1.maven.org/maven2/io/swagger/swagger-codegen-cli/2.4.18/swagger-codegen-cli-2.4.18.jar"
+
+
 printUsage() {
   printf "usage:\n"
   printf "$0 download <apiVersion: git-hash-or-branchname> \t - downloads the api yaml file with given api version\n"
@@ -17,6 +20,7 @@ printUsage() {
 downloadApi() {
   echo "download ${apiVersion}"
   wget -N -nv -P "${SCRIPTDIR}" "${apiUrl}"
+  wget -N -nv -O swagger-codegen-cli.jar -P "${SCRIPTDIR}" "${swaggerCodeGen}"
 }
 
 convertToV3() {
@@ -25,25 +29,8 @@ convertToV3() {
 }
 
 generate() {
-  # create body as raw
-  get_post_data() {
-  cat <<EOF
-{
-  "swaggerUrl": "${apiUrl}"
-}
-EOF
-}
-
   echo "generate code for version ${apiVersion}"
-  RESPONSE=$(curl -s \
-    -H "Accept: application/json" \
-    -H "Content-Type: application/json" \
-    -X POST -d "$(get_post_data)" https://generator.swagger.io/api/gen/clients/typescript-fetch
-  )
-  LINK=$(echo "${RESPONSE}" | jq -r '.link')
-  $(wget -nv -O "stubs.zip" "${LINK}")
-  unzip -o stubs.zip
-  rm stubs.zip
+  java -jar swagger-codegen-cli.jar generate -i api.swagger.yaml -l typescript-fetch -o ./typescript-fetch-client
   cd  typescript-fetch-client
   grep -n -A 64 "import \* as url" api.ts | sed -n 's/^\([0-9]\{1,\}\).*/\1d/p' | sed -f - api.ts  > api.tmp
   grep -n -A 10000  -B 4 "export const " api.tmp  | sed -n 's/^\([0-9]\{1,\}\).*/\1d/p'  | sed -f - api.tmp > api2.tmp
