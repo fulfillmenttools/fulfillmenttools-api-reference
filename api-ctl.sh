7147e8b3268f434d331225061d617bbff45133bb
Ralf Bettermann <ralf.bettermann@fulfillmenttools.com>
2025-09-08 11:53:44 +0200

    move to openapi generator

--- api-ctl.sh
+++ api-ctl.sh
@@ -7,14 +7,13 @@ apiVersion=$2
 
 apiUrl="https://raw.githubusercontent.com/fulfillmenttools/fulfillmenttools-api-reference/${apiVersion}/api.swagger.yaml"
 
-swaggerCodeGen="https://repo1.maven.org/maven2/io/swagger/swagger-codegen-cli/2.4.18/swagger-codegen-cli-2.4.18.jar"
+openapigenerator="https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/7.14.0/openapi-generator-cli-7.14.0.jar"
 
 
 printUsage() {
   printf "usage:\n"
   printf "$0 download <apiVersion: git-hash-or-branchname> \t - downloads the api yaml file with given api version\n"
   printf "$0 generate <apiVersion: git-hash-or-branchname> \t - generates typescript code for the api with the given api version\n"
-  printf "$0 convertToV3 <apiVersion: git-hash-or-branchname> \t - converts / the api (v2) to open API v3 format\n"
 }
 
 downloadApi() {
@@ -22,17 +21,13 @@ downloadApi() {
   wget -N -nv -P "${SCRIPTDIR}" "${apiUrl}"
 }
 
-convertToV3() {
-  echo "converting the swagger v2 API with version: ${apiVersion} to Open API v3 format"
-  wget -nv -P "${SCRIPTDIR}" https://converter.swagger.io/api/convert?url="${apiUrl}" -O openapiv3.yaml
-}
-
 generate() {
   echo "generate code for version ${apiVersion}"
-  wget -N -nv -P "${SCRIPTDIR}" "${swaggerCodeGen}" -O "${SCRIPTDIR}/swagger-codegen-cli.jar"
+  wget -N -nv -P "${SCRIPTDIR}" "${openapigenerator}" -O "${SCRIPTDIR}/openapi-generator.jar"
+  mkdir -p typescript-fetch-client
   downloadApi
-  java -jar swagger-codegen-cli.jar generate -i api.swagger.yaml -l typescript-fetch -o ./typescript-fetch-client
-  cd  typescript-fetch-client
+  java -jar "${SCRIPTDIR}/openapi-generator.jar" generate -g typescript-fetch -i "${SCRIPTDIR}/api.swagger.yaml" -o ./typescript-fetch-client --verbose
+  cd typescript-fetch-client
   grep -n -A 64 "import \* as url" api.ts | sed -n 's/^\([0-9]\{1,\}\).*/\1d/p' | sed -f - api.ts  > api.tmp
   grep -n -A 10000  -B 4 "export const " api.tmp  | sed -n 's/^\([0-9]\{1,\}\).*/\1d/p'  | sed -f - api.tmp > api2.tmp
   tail -n +2 api2.tmp > api.ts
@@ -48,7 +43,6 @@ main() {
   case ${command} in
     "download") downloadApi ;;
     "generate") generate ;;
-    "convertToV3") convertToV3 ;;
     *) printUsage; exit 0 ;;
   esac
 }
